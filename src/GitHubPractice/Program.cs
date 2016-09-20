0e23246bd2ae51d2bc78bea77351eddd979595eb
danielvivacqua <drvivacqua@hotmail.com>
2016-09-20 16:01:37 -0400

    first commit

---
+++ src/GitHubPractice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitHubPractice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub Practice!");
+        }
+    }
+}
